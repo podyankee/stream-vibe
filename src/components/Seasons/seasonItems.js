1c23f5d86c3ab4a07eeff7a6ab947d9a11029540
podyankee <podyankee@gmail.com>
2026-03-22 18:50:23 +0300

    show page finished

---
+++ src/components/Seasons/seasonItems.js
@@ -0,0 +1,418 @@
+const seasonItems = [
+  {
+    title: 'Season 01',
+    subtitle: '9 Episoddes',
+    episodes: [
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Season 02',
+    subtitle: '5 Episoddes',
+    episodes: [
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Season 03',
+    subtitle: '3 Episoddes',
+    episodes: [
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+      {
+        number: '01',
+        title: 'Chapter One : The Vanishing of Will Byers',
+        description:
+          'On his way from a friend’s house, young Will sees something terrifying . Nearby, a sinister secret lurks in the depths of a government lab.',
+        duration: '49 min',
+        video: {
+          src: '/videos/sample.mp4',
+          poster: '/video-posters/1.jpg',
+        },
+      },
+    ],
+  },
+]
+
+export default seasonItems
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import InputMaskCollection from '@/modules/InputMaskCollection'
 import OverlayMenu from '@/modules/OverlayMenu'
 import SliderCollection from '@/modules/SliderCollection'
 import TabsCollection from '@/modules/TabsCollection'
+import VideoPlayerCollection from '@/modules/VideoPlayerCollection'
 
 new OverlayMenu()
 new InputMaskCollection()
 new SliderCollection()
 new TabsCollection()
+new VideoPlayerCollection()
