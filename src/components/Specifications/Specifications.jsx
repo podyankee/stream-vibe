942eda03750868fb5d589a7990502ad228295f4a
podyankee <podyankee@gmail.com>
2026-04-29 15:31:28 +0300

    subscriptions page finished

---
+++ src/components/Specifications/Specifications.jsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx'
+import './Specifications.scss'
+
+const Specifications = (props) => {
+  const { items = [] } = props
+
+  return (
+    <div className="specifications">
+      <dl className="specifications__list">
+        {items.map(({ key, value, isWide }, index) => (
+          <div
+            className={clsx('specifications__item', {
+              'specifications__item--wide': isWide,
+            })}
+            key={index}
+          >
+            <dt className="specifications__key">{key}</dt>
+            <dd className="specifications__value">{value}</dd>
+          </div>
+        ))}
+      </dl>
+    </div>
+  )
+}
+export default Specifications
